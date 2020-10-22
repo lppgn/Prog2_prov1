a600a475eb4c78317db8413e5c16dd7b82bcf4b6
lppgn <33622086+lppgn@users.noreply.github.com>
2020-10-22 15:09:53 +0200

    First Commit

---
+++ Prog2pvor1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog2pvor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
